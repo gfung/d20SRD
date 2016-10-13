0f5d7098b0e17eb640ad69dcd1c7420e0dae1393
George Fung <gycf84@yahoo.com>
2016-10-13 14:44:18 -0700

    Create example.js

---
+++ models/example.js
@@ -0,0 +1,35 @@
+let barbarian={
+	hd: 12,
+	armor:["light", "shields"],
+	weapons:["simple", "martial"]
+	tools: [],
+	saving_throws: ["str", "con"],
+	skills:{
+		count: 2,
+		list: [ "Animal Handling", "Athletics", "Intimidation", "Nature", "Perception", "Survival" ]
+	},
+	proficiency_bonus:[0,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6],
+	features:[
+		["Rage", "Unarmored Defense"], //1
+		["Reckless Attack", "Danger Sense"], //2
+		["Primal Path"], //3
+		["Ability Score Improvement"], //4
+		["Extra Attack", "Fast Movement"], //5
+		["Path Feature"], //6
+		["Feral Instinct"]. //7
+		["Ability Score Improvement"], //8
+		["Brutal Critical"], //9
+		["Path Feature"], //10
+		["Relentless Rage"],  //11
+		["Ability Score Improvement"], //12
+		["Brutal Critical"], //13
+		["Path Feature"], //14
+		["Persistant Rage"], //15
+		["Ability Score Improvement"], //16
+		["Brutal Critical"], //17
+		["Indomitable Might"], //18
+		["Ability Score Improvement"], //19
+		["Primal Champion"], //20
+	],
+
+}
